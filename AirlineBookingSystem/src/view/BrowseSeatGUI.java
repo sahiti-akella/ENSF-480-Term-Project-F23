0809ff106bca65ccc66ac8e4c0f22196de6406dc
Sahitiakella <sahiti.akella@gmail.com>
2023-11-27 20:33:10 -0700

    Seat Availability map works

--- AirlineBookingSystem/src/view/BrowseSeatGUI.java
+++ AirlineBookingSystem/src/view/BrowseSeatGUI.java
@@ -4,11 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BrowseSeatGUI {
     private JFrame frame;
     private JPanel seatPanel;
     private String selectedFlight;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/FRWA";
+    private static final String USER = "root";
+    private static final String PASSWORD = "sahi2004";
+    private static final int SEATS_PER_FLIGHT = 12;
 
     public BrowseSeatGUI(String selectedFlight) {
         this.selectedFlight = selectedFlight;
@@ -23,8 +32,10 @@ public class BrowseSeatGUI {
         seatPanel = new JPanel();
         seatPanel.setLayout(new GridLayout(3, 4)); // Change the dimensions as needed
 
+        // Calculate the starting seat number based on the flight
+        int startingSeatNumber = (getFlightID(selectedFlight) - 1) * SEATS_PER_FLIGHT + 1;
+
         // Create seats
-        int seatNumber = 1;
         for (int row = 1; row <= 3; row++) {
             String seatType;
             if (row == 1) {
@@ -36,15 +47,64 @@ public class BrowseSeatGUI {
             }
 
             for (int col = 1; col <= 4; col++) {
-                JButton seatButton = new JButton(seatType + " - Seat " + seatNumber);
-                seatButton.addActionListener(new SeatClickListener());
+                int seatNumber = startingSeatNumber + (row - 1) * 4 + col - 1;
+                JButton seatButton = createSeatButton(seatType, seatNumber);
                 seatPanel.add(seatButton);
-                seatNumber++;
             }
         }
 
         frame.add(seatPanel);
         frame.setVisible(true);
+    }
+
+    private JButton createSeatButton(String seatType, int seatNumber) {
+        JButton seatButton = new JButton(seatType + " - Seat " + seatNumber);
+        seatButton.addActionListener(new SeatClickListener());
+
+        // Check seat availability and enable/disable the button accordingly
+        if (!isSeatAvailable(seatNumber)) {
+            seatButton.setEnabled(false);
+        }
+
+        return seatButton;
+    }
+
+    private boolean isSeatAvailable(int seatNumber) {
+        try {
+            // Query the database to check seat availability for the selected flight
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            String query = "SELECT IsAvailable FROM SEATS WHERE FlightID = ? AND SeatID = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, getFlightID(selectedFlight));
+            statement.setInt(2, seatNumber);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                int isAvailable = resultSet.getInt("IsAvailable");
+                return isAvailable == 1; // Assuming 1 represents available and 0 represents not available
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false; // Default to not available in case of an error
+    }
+
+    private int getFlightID(String selectedFlight) {
+        // Query the database to get the FlightID for the selected flight
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            String query = "SELECT FlightID FROM FLIGHTS WHERE CONCAT(Origin, ' -> ', Destination, ' : ', DepartureDate) = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, selectedFlight);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getInt("FlightID");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1; // Return -1 in case of an error
     }
 
     private class SeatClickListener implements ActionListener {
